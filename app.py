a0b4e6274216cc60fa120c5841cbcfab234c41fe
Nguyễn Gia Phong <mcsinyx@disroot.org>
2020-10-20 18:18:22 +0700

    Prototype registration

--- app.py
+++ app.py
@@ -7,47 +7,57 @@ from flask_login import (LoginManager, UserMixin, current_user,
                          login_required, login_user, logout_user)
 
 with open('static/login.html') as f: LOGIN_HTML = f.read()
+with open('static/register.html') as f: REGISTER_HTML = f.read()
 
 app = Flask(__name__)
 app.secret_key = token_bytes()
 login_manager = LoginManager(app)
-users = {'foo@bar.tld': {'password': crypt('secret')}}
+users = {}
 
 
 class User(UserMixin):
     pass
 
 
-def authenticate(email, password):
-    if email not in users: return False
-    digest = users[email]['password']
+def authenticate(username, password):
+    if username not in users: return False
+    digest = users[username]['password']
     return compare_digest(digest, crypt(password, digest))
 
 
 @login_manager.user_loader
-def load_user(email):
-    if email not in users: return None
+def load_user(username):
+    if username not in users: return None
     user = User()
-    user.id = email
+    user.id = username
     return user
 
 
 @login_manager.request_loader
 def load_request(request):
-    email = request.form.get('email')
-    is_authenticated = authenticate(email, request.form['password'])
+    username = request.form.get('username')
+    is_authenticated = authenticate(username, request.form['password'])
     user = User()
-    user.id, user.is_authenticated = email, is_authenticated
+    user.id, user.is_authenticated = username, is_authenticated
     return user
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET': return REGISTER_HTML
+    username = request.form['username']
+    password = crypt(request.form['password'])
+    users[username] = {'password': password}
+    return redirect(url_for('login'))
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET': return LOGIN_HTML
-    email = request.form['email']
-    if not authenticate(email, request.form['password']): return 'Bad login'
+    username = request.form['username']
+    if not authenticate(username, request.form['password']): return 'Bad login'
     user = User()
-    user.id = email
+    user.id = username
     login_user(user)
     return redirect(url_for('protected'))
 
