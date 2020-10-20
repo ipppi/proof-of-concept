633ce8dfbaf78421eae72dec7465e8be455dc204
Nguyễn Gia Phong <mcsinyx@disroot.org>
2020-10-21 00:13:16 +0700

    Integrate PostgreSQL

--- app.py
+++ app.py
@@ -5,60 +5,73 @@ from secrets import token_bytes
 from flask import Flask, redirect, request, url_for
 from flask_login import (LoginManager, UserMixin, current_user,
                          login_required, login_user, logout_user)
+from pg8000 import connect
 
 with open('static/login.html') as f: LOGIN_HTML = f.read()
 with open('static/register.html') as f: REGISTER_HTML = f.read()
 
-app = Flask(__name__)
-app.secret_key = token_bytes()
-login_manager = LoginManager(app)
-users = {}
+
+class AccountData:
+    def __init__(self, con):
+        self.con = con
+        self.con.run(
+            'CREATE TEMPORARY TABLE account (username TEXT, password TEXT)')
+
+    def user_exists(self, username):
+        return bool(self.con.run(
+            f"SELECT username FROM account WHERE username='{username}'"))
+
+    def add(self, username, password):
+        if self.user_exists(username): return False
+        con.run('INSERT INTO account (username, password)'
+                ' VALUES (:username, :password)',
+                username=username, password=crypt(password))
+        return True
+
+    def authenticate(self, username, password):
+        passwords = self.con.run(
+            f"SELECT password FROM account WHERE username='{username}'")
+        try:
+            digest = passwords[0][0]
+        except IndexError:
+            return False
+        else:
+            return compare_digest(digest, crypt(password, digest))
 
 
 class User(UserMixin):
-    pass
+    def __init__(self, username):
+        self.id = username
 
 
-def authenticate(username, password):
-    if username not in users: return False
-    digest = users[username]['password']
-    return compare_digest(digest, crypt(password, digest))
+app = Flask(__name__)
+app.secret_key = token_bytes()
+login_manager = LoginManager(app)
+con = connect('postgres', password='postgres')
+accounts = AccountData(con)
 
 
 @login_manager.user_loader
 def load_user(username):
-    if username not in users: return None
-    user = User()
-    user.id = username
-    return user
-
-
-@login_manager.request_loader
-def load_request(request):
-    username = request.form.get('username')
-    is_authenticated = authenticate(username, request.form['password'])
-    user = User()
-    user.id, user.is_authenticated = username, is_authenticated
-    return user
+    if not accounts.user_exists(username): return None
+    return User(username)
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'GET': return REGISTER_HTML
-    username = request.form['username']
-    password = crypt(request.form['password'])
-    users[username] = {'password': password}
-    return redirect(url_for('login'))
+    if accounts.add(request.form['username'], request.form['password']):
+        return redirect(url_for('login'))
+    return 'username already exists'
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET': return LOGIN_HTML
     username = request.form['username']
-    if not authenticate(username, request.form['password']): return 'Bad login'
-    user = User()
-    user.id = username
-    login_user(user)
+    if not accounts.authenticate(username, request.form['password']):
+        return 'Bad login'
+    login_user(User(username))
     return redirect(url_for('protected'))
 
 
