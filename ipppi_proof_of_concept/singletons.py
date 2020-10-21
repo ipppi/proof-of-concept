3eff7b6ec613e56767e4270beaae4e0f51165ed1
Nguyễn Gia Phong <mcsinyx@disroot.org>
2020-10-21 10:58:52 +0700

    Add license headers

--- ipppi_proof_of_concept/singletons.py
+++ ipppi_proof_of_concept/singletons.py
@@ -1,3 +1,21 @@
+# Singletons for convenient use (it's ugly I know)
+# Copyright (C) 2020  Nguyễn Gia Phong
+#
+# This file is part of IPPPI.
+#
+# IPPPI is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# IPPPI is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with IPPPI.  If not, see <https://www.gnu.org/licenses/>.
+
 from secrets import token_bytes
 
 from flask import Flask
--- ipppi_proof_of_concept/static/__init__.py
+++ ipppi_proof_of_concept/static/__init__.py
@@ -1,3 +1,21 @@
+# Static data
+# Copyright (C) 2020  Nguyễn Gia Phong
+#
+# This file is part of IPPPI.
+#
+# IPPPI is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# IPPPI is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with IPPPI.  If not, see <https://www.gnu.org/licenses/>.
+
 from importlib.resources import path
 
 with path('ipppi_proof_of_concept.static', 'register.html') as p:
